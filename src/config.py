a3baa5930e6f93d9a5ffd5978f1df86ae5c90165
isayhc <isayahculbertson@gmail.com>
2023-09-09 10:53:34 -0400

    removed the hardcoded string  in src/config

--- src/config.py
+++ src/config.py
@@ -1,8 +1,11 @@
+import os
+wand_db_username = os.environ.get("WANDDB_USERNAME")
+
 config = {
     'project': "llmapps",
     'entity': None,
     'job_type': "production",
-    'vector_store_artifact': "[INSERT-YOUR-USERNAME]/llmapps/vector_store:latest",
+    'vector_store_artifact': f"{wand_db_username}/llmapps/vector_store:latest",
     'model_name':"gpt-3.5-turbo",
     'chat_temperature': 0.3,
     'max_fallback_retries': 1
